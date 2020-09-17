32c0afb3c9193543beacfa4a7a8be8d47aa9417e
Shulha Nikita <shulgamalthael@gmail.com>
2020-09-17 18:22:24 +0300

    init files

--- Lesson-1/task3/webpack.config.js
+++ Lesson-1/task3/webpack.config.js
@@ -1,21 +1,26 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require('path');
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /.js$/,
-                use: ["babel-loader"]
-            },
-            {
-                test: /.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'review_build')
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /.css$/,
+        use: ["style-loader", "css-loader"]
+      }
     ]
-};
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+}
