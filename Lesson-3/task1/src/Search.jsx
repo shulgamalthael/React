b61f5105ff20ccd62aa0e70ca096a1d6afeff05c
Shulha Nikita <shulgamalthael@gmail.com>
2020-09-21 16:56:22 +0300

    init files

--- Lesson-3/task1/src/Search.jsx
+++ Lesson-3/task1/src/Search.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-const rootElement = document.querySelector('#root');
-
-export const Search = (props) => {
-const element = (
-  <div class="search">
-      <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
-      <div className="search__field">
-          <input type="text" className="search__input" />
-          <button className="search__button">Search</button>
-      </div>
-  </div>
-);
-
-return element;
+export default function Search(props) {
+    // console.log(props)
+    return (
+        <div className='searh'>
+            <h1 className='search__title'>{`Hello, ${props.name}. What to search for you?`}</h1>
+            <div className='search__field'>
+                <input type='text' className='search__input' />
+                <button className='search__button'>Search</button>
+            </div>
+        </div>
+    );
 };
 
-ReactDOM.render(<Search name="Nikita" />, rootElement);
+// export default Search;
