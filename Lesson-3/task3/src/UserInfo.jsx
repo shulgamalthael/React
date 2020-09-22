425afe015c67783687e7c58813fca6d8389fbdc0
Shulha Nikita <shulgamalthael@gmail.com>
2020-09-22 16:32:27 +0300

    mod

--- Lesson-3/task3/src/UserInfo.jsx
+++ Lesson-3/task3/src/UserInfo.jsx
@@ -5,8 +5,8 @@ const UserInfo = (props) => {
     return (
         <div className="user-info">
             <UserAvatar 
-                avatarUrl={props.avatarUrl} 
-                name={props.name}  />
+                avatarUrl={props.user.avatarUrl} 
+                name={props.user.name}  />
             <div className="user-info__name">
                 {props.name}
             </div>
