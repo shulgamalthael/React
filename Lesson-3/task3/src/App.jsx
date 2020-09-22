7ebc5ca794b1c32d8c23223fceb6d31937652c29
Shulha Nikita <shulgamalthael@gmail.com>
2020-09-22 18:32:57 +0300

    mod

--- Lesson-3/task3/src/App.jsx
+++ Lesson-3/task3/src/App.jsx
@@ -9,7 +9,7 @@ const userInfo =  {
 const App = () => {
     return (
         <Comment
-            user = {userInfo}
+            author = {userInfo}
             text = "Good Job!"
             date = {new Date('2019-01-01T11:32:19.566Z')}
         />
