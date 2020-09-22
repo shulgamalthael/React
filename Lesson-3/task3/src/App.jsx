08be4b6bbb9966ca1032e1e28364e0c4eb96b24e
Shulha Nikita <shulgamalthael@gmail.com>
2020-09-22 16:27:06 +0300

    mod

--- Lesson-3/task3/src/App.jsx
+++ Lesson-3/task3/src/App.jsx
@@ -3,7 +3,7 @@ import Comment from './Comment';
 
 const userInfo = {
     name: "Tom",
-    avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQVK7xUD2P-8Dqt-CDb1JLDHzE_2yAns-AttA&usqp=CAU'
+    avatarUrl: "https://avatars1.githubusercontent.com"
 };
 
 const App = () => {
