7da78233a74806aef35d337f34215f349ed0375f
Shulha Nikita <shulgamalthael@gmail.com>
2020-09-24 15:22:03 +0300

    init files

---
+++ Lesson-5/task3/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Counter from './Counter';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(
+    <Counter />,
+    rootElement
+);
