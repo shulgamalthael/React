59c1d47d2a299d3e5f56b6bff9a979e16ecf4ab6
Shulha Nikita <shulgamalthael@gmail.com>
2020-10-01 13:03:21 +0300

    init files

--- Lesson-8/task1/src/Life.jsx
+++ Lesson-8/task1/src/Life.jsx
@@ -1,34 +1,32 @@
-import React, {Component} from 'react';
+import React from "react";
 
-class Life extends Component {
-    constructor(props) {
-        super(props);
-        console.log('constructor: good place to create state');
-    }
+class Life extends React.Component {
+  constructor(props){
+    super(props);
+    console.log('constructor: good place to create state')
+  }
+  
+  componentDidMount(){
+    console.log('componentDidMount: API calls, subscriptions')
+  }
 
-    componentDidMount() {
-        console.log('componentDidMount: API calls, subscriptions');
-    }
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render')
+    return this.props !== nextProps
+  }
 
-    componentWillUnmount() {
-        console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
-    }
+  componentDidUpdate(prevProps, prevState){
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props')
+  }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log(nextProps, nextState);
-        console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
-        return nextProps.number % 2;
-    }
+  componentWillUnmount(){
+    console.log('componentWillUnmount(): cleanup before DOM related to component will be removed')
+  }
 
-    componentDidUpdate(prevProps, prevState) {
-        console.log(prevProps, prevState);
-        console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
-    }
-
-    render() {
-        console.log('return React element to build DOM');
-    return <div className="number"></div>
-    }
+  render(){
+    console.log('return React element to build DOM')
+    return <div></div>
+  }
 }
 
 export default Life;
