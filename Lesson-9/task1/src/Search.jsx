9377ecdf339c90179e5cef8b4c3ef903b63ea818
Shulha Nikita <shulgamalthael@gmail.com>
2020-10-02 13:21:26 +0300

    init files

---
+++ Lesson-9/task1/src/Search.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Search extends React.Component {
+    state = { value: 'good restaurant' }
+
+    handleChange = event => {
+        this.setState({
+            value: event.target.value
+        });
+    }
+
+    search = event => {
+        event.preventDefault();
+        alert('Search text:' + this.state.value);
+    }
+
+    render() {
+        return (
+            <form className="search" onSubmit={this.search}>
+                <input type="text" onChange={this.handleChange} value={this.state.value} className="search__input" />
+                <button className="search__button" type="submit">Search</button>
+            </form>
+        )
+    }
+}
+
+export default Search;
