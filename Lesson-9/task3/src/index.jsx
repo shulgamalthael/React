4390d84705c3cbadae5d32f2cd97940f140ee9f5
Shulha Nikita <shulgamalthael@gmail.com>
2020-10-04 15:51:23 +0300

    init files

---
+++ Lesson-9/task3/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import UserFrom from './UserForm';
+import './index.scss';
+
+ReactDOM.render(
+    <UserFrom />,
+    document.querySelector('#root')
+);
