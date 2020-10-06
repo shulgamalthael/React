59225bdd916a63dd6a1c024f4fecc700159b3d06
Shulha Nikita <shulgamalthael@gmail.com>
2020-10-06 13:25:50 +0300

    init files

---
+++ Lesson-10/hw2/src/SearchBar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  handleFilterTextChange = (e) => {
+    this.props.onFilterTextChange(e.target.value);
+  };
+
+  handleInStockChange = (e) => {
+    this.props.onInStockChange(e.target.checked);
+  };
+
+  render() {
+    const { filterText, inStockOnly } = this.props;
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={filterText}
+          onChange={this.handleFilterTextChange}
+        />
+        <p>
+          <input
+            type="checkbox"
+            checked={inStockOnly}
+            onChange={this.handleInStockChange}
+          />{" "}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
+export default SearchBar
